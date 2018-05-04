f37b9be559235010a0f8b939348123eeb2f874b8
huliyou <liyoupie@gmail.com>
2018-05-04 23:47:09 +0800

    fix mock

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,5 +1,12 @@
-const mock = {}
-require('fs').readdirSync(require('path').join(__dirname + '/src/mock')).forEach(function(file) {
-	Object.assign(mock, require('./src/mock/' + file))
-})
-module.exports = mock
+// const mock = {}
+// const noProxy = process.env.NO_PROXY === 'true';
+// require('fs').readdirSync(require('path').join(__dirname + '/src/mock')).forEach(function(file) {
+// 	Object.assign(mock, require('./src/mock/' + file))
+// })
+// module.exports = mock
+// export default noProxy ? {} : delay(proxy, 1000);
+
+export default {
+  '/api/*': 'http://47.104.163.55:8081',
+  'POST /api/*': 'http://47.104.163.55:8081',
+};
