164350e516ff6973e0f3a9ff390c89acf625e8b7
huliyou <liyoupie@gmail.com>
2018-05-11 17:16:45 +0800

    参数管理操作按钮

--- src/routes/paramsManage/index.js
+++ src/routes/paramsManage/index.js
@@ -30,16 +30,28 @@ const Index = ({ user, paramsBrowse, dispatch, loading, location, paramsManage }
     { title: '参数组名称', dataIndex: 'userParamsetName', key: 'userParamsetName' },
     { title: '是否必判', dataIndex: 'isEssential', key: 'isEssential' },
     { title: '所属型号', dataIndex: 'modelName', key: 'modelName' },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+      key: 'operation',
+      render: () => (
+        <span className="table-operation">
+          <Button type="primary">编辑</Button>
+          <Button style={{ margin: '0 5px' }}>保存</Button>
+          <Button type="danger">删除</Button>
+        </span>
+      ),
+    },
   ];
   let data = paramsManage.list;
   data = data && data.map((item, key) => ({ ...item, key }));
-  const expandedRowRender = () => {
+  const expandedRowRender = (row) => {
     const columns = [
-      { title: '参数代号', dataIndex: 'date', key: 'date' },
-      { title: '参数名称', dataIndex: 'name', key: 'name' },
+      { title: '参数代号', dataIndex: 'paramCode', key: 'paramCode' },
+      { title: '参数名称', dataIndex: 'paramName', key: 'paramName' },
       { title: '所属系统', dataIndex: 'upgradeNum', key: 'state' },
-      { title: '所属参数集', dataIndex: 'upgradeNum', key: 'upgradeNum' },
-      { title: '所属设备', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+      { title: '所属参数集', dataIndex: 'paramsetName', key: 'paramsetName' },
+      { title: '所属设备', dataIndex: 'device', key: 'device' },
       // { title: '操作', dataIndex: 'upgradeNum', key: 'upgradeNum' },
       {
         title: 'Action',
@@ -59,15 +71,7 @@ const Index = ({ user, paramsBrowse, dispatch, loading, location, paramsManage }
       },
     ];
 
-    const data = [];
-    for (let i = 0; i < 3; ++i) {
-      data.push({
-        key: i,
-        date: '2014-12-24 23:12:00',
-        name: 'This is production name',
-        upgradeNum: 'Upgraded: 56',
-      });
-    }
+    const data = row.listParamSelectDTO || [];
     return (
       <Table
         columns={columns}
